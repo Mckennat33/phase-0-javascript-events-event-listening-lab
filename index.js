6d81b869fcbb07b78eec4f710a47f443b7e8d8c9
thomasmckenna <thomasmckenna12@gmail.com>
2021-07-26 14:20:12 -0400

    /Users/thomasmckenna/phase-0-javascript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let input = document.getElementById('input')
+
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        alert('I was clicked!'); 
+    }); 
 }
+
+
+//  const input = document.getElementById('input');
+//  
+//  input.addEventListener('click', function() {
+//    alert('I was clicked!');
+//  });
