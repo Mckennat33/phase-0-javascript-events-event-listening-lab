e18e218d1e6e8f74a52303c9e53489df144157b8
Thomas <thomasmckenna12gmail.com>
2021-08-25 17:51:34 -0400

    commit tree ef476a1f713a5ea227826f2c4f806fc4627394d5

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+ 
+ // let input = document.getElementById('input') 
+ //   function clickAlert() { 
+ //       input.addEventListener('click', function() { 
+ //               alert('I was clicked!');  
+ //     });  
+  
+     
+     
+//      const input = document.getElementById('input');
+//   
+//   input.addEventListener('click', function() {
+//     alert('I was clicked!');  
+// });
+
+// Phase 1
+
+// const input = document.getElementById('input');
+// 
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+
 let input = document.getElementById('input')
 
 function addingEventListener() {
@@ -5,10 +30,3 @@ function addingEventListener() {
         alert('I was clicked!'); 
     }); 
 }
-
-
-//  const input = document.getElementById('input');
-//  
-//  input.addEventListener('click', function() {
-//    alert('I was clicked!');
-//  });
